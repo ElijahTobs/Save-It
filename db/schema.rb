8de26ffee7eb27b89f20745c36d24df54ae5ae8b
Elijah <eligenius@gmail.com>
2020-10-15 21:39:56 +0100

    Generate users controller

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_10_15_144055) do
+ActiveRecord::Schema.define(version: 2020_10_15_152042) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,12 @@ ActiveRecord::Schema.define(version: 2020_10_15_144055) do
     t.integer "user_id"
     t.string "name"
     t.float "amount"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
