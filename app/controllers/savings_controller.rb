e3f2d6d7d13b3310afa75e91b2692caf711c840a
Elijah <eligenius@gmail.com>
2020-10-15 15:44:44 +0100

    Populate saving controller and views

--- app/controllers/savings_controller.rb
+++ app/controllers/savings_controller.rb
@@ -1,2 +1,64 @@
 class SavingsController < ApplicationController
+  before_action :set_saving, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @savings = Saving.all
+  end
+
+  def show
+    
+  end
+
+  def new
+    @saving = Saving.new
+  end
+
+  def edit
+    
+  end
+
+  def create
+    @saving = Saving.new(saving_params)
+
+    respond_to do |format|
+      if @saving.save
+        format.html { redirect_to @saving, notice: 'Saving was successfully created.' }
+        format.json { render :show, status: :created, location: @saving }
+      else
+        format.html { render :new }
+        format.json { render json: @saving.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+
+      if @saving.update(saving_params)
+        format.html { redirect_to @saving, notice: 'Saving was successfully updated.' }
+        format.json { render :show, status: :ok, location: @saving }
+      else
+        format.html { render :edit }
+        format.json { render json: @saving.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @saving.destroy
+    respond_to do |format|
+
+      format.html { redirect_to savings_url, notice: 'Saving was successfully destroyed.' }
+      format.json { head :no_content }
+  end
+
+  private
+    def set_saving
+      @saving = Saving.find(params[:id])
+    end
+
+    def saving_params
+      params.require(:saving).permit(:user_id, :name, :amount)
+    end
+  end
 end
---
+++ db/migrate/20201015144055_create_savings.rb
@@ -0,0 +1,12 @@
+class CreateSavings < ActiveRecord::Migration[5.2]
+  def change
+    create_table :savings do |t|
+
+      t.integer :user_id
+      t.string :name
+      t.float :amount
+
+      t.timestamps
+    end
+  end
+end
