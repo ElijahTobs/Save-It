03b2a44ca0e9bea655862fbd9c1a7fceb1fb7578
Elijah <eligenius@gmail.com>
2020-10-17 21:10:18 +0100

    Populate users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,65 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+  before_action :check_login, except: %i[index new create show]
+
+  def index
+    @user = User.new
+    render 'sign_in'
+  end
+
+  def show; end
+
+  def new
+    @user = User.new
+  end
+
+  def edit; end
+
+  def create
+    @user = User.new(user_params)
+
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    respond_to do |format|
+      if @user.update(user_params)
+        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: @user }
+      else
+        format.html { render :edit }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :avatar_url)
+  end
+
+  def check_login
+    redirect_to root_path if session[:user_name].nil?
+  end
 end
