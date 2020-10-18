d791d529ef86b857a6d71f62c425a5715e1aebf2
Elijah <eligenius@gmail.com>
2020-10-18 22:33:26 +0100

    Update groups controller

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -1,6 +1,6 @@
 class GroupsController < ApplicationController
-  before_action :set_group, only: [:show, :edit, :update, :destroy]
-  
+  before_action :set_group, only: %i[show edit update destroy]
+
   # GET /groups
   # GET /groups.json
   def index
@@ -10,20 +10,22 @@ class GroupsController < ApplicationController
   # GET /groups/1
   # GET /groups/1.json
   def show
-    @group_savings = @group.savings.ordered_by_most_recent
+    @group_expenses = @group.expenses.ordered_by_most_recent
   end
 
   # GET /groups/new
   def new
     @group = Group.new
   end
+
   # GET /groups/1/edit
-  def edit
-  end
+  def edit; end
+
   # POST /groups
   # POST /groups.json
   def create
     @group = Group.new(group_params)
+
     respond_to do |format|
       if @group.save
         format.html { redirect_to @group, notice: 'Group was successfully created.' }
@@ -33,5 +35,41 @@ class GroupsController < ApplicationController
         format.json { render json: @group.errors, status: :unprocessable_entity }
       end
     end
+  end
+
+  # PATCH/PUT /groups/1
+  # PATCH/PUT /groups/1.json
+  def update
+    respond_to do |format|
+      if @group.update(group_params)
+        format.html { redirect_to @group, notice: 'Group was successfully updated.' }
+        format.json { render :show, status: :ok, location: @group }
+      else
+        format.html { render :edit }
+        format.json { render json: @group.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /groups/1
+  # DELETE /groups/1.json
+  def destroy
+    @group.destroy
+    respond_to do |format|
+      format.html { redirect_to groups_url, notice: 'Group was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_group
+    @group = Group.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def group_params
+    params.require(:group).permit(:creator_id, :name, :icon)
   end
 end
