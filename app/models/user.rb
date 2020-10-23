b0079831b742f363806a456ab143d5b3caff5be6
Elijah <eligenius@gmail.com>
2020-10-23 08:56:56 +0100

    Add name format validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   validates :name, uniqueness: true
   validates :name, presence: true
+  validates :name, format: { with: /[a-zA-Z0-9]/ }
 
   has_many :savings, foreign_key: 'author_id', class_name: 'Saving'
   has_many :groups, foreign_key: 'creator_id', class_name: 'Group'
