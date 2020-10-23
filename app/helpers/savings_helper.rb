27248ad987970ed8328963a166b08543ee158bb3
Elijah <eligenius@gmail.com>
2020-10-23 17:17:43 +0100

    Add helper methods for groups and savings

--- app/helpers/savings_helper.rb
+++ app/helpers/savings_helper.rb
@@ -1,2 +1,17 @@
 module SavingsHelper
+  def get_group_icon(exp)
+    if exp.groups.first.nil?
+      exp.author.avatar_url
+    else
+      exp.groups.first.icon
+    end
+  end
+
+  def savings_rendering(exps)
+    render partial: 'layouts/savinglist', locals: { savings: exps } unless exps.nil?
+  end
+
+  def total_savings(exps)
+    exps.total_amount
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'users#index'
-
+  resources :groups, except: [:update, :edit]
+  resources :savings, except: [:update, :edit]
+  resources :users, except: [:update, :edit]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  # Custom endpoints for the app
   get '/savings/new/:group', to: 'savings#new'
 
   post '/access', to: 'access_control#signin'
-  delete '/access', to: 'access_control#logout'
+  get '/access', to: 'access_control#logout'
 
   get '/mysavings', to: 'savings#index'
-  get '/groups', to: 'groups#index'
-  # get '/groups/new', to: 'groups#new'
+  get '/myexternal', to: 'savings#my_external'
 
-  
-
-  resources :groups, except: [:update]
-  resources :savings, except: [:update]
-  resources :users, except: [:update, :edit]
+  # Custom root page for the application
+  root 'application#index'
 end
