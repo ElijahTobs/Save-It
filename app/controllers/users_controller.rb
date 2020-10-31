36775166745038452ceadeea91be3260b5edbd8f
Elijah <eligenius@gmail.com>
2020-10-31 14:01:03 +0100

    Remove more commented codes

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,25 +2,17 @@ class UsersController < ApplicationController
   before_action :set_user, only: %i[show edit update destroy]
   before_action :check_login, except: %i[index new create show]
 
-  # GET /users
-  # GET /users.json
   def index
-    # @users = User.all
     @user = User.new
     render 'sign_in'
   end
 
-  # GET /users/1
-  # GET /users/1.json
   def show; end
 
-  # GET /users/new
   def new
     @user = User.new
   end
 
-  # POST /users
-  # POST /users.json
   def create
     @user = User.new(user_params)
 
@@ -36,8 +28,6 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     respond_to do |format|
       if @user.destroy
@@ -50,12 +40,10 @@ class UsersController < ApplicationController
 
   private
 
-  # Use callbacks to share common setup or constraints between actions.
   def set_user
     @user = User.find(params[:id])
   end
 
-  # Only allow a list of trusted parameters through.
   def user_params
     params.require(:user).permit(:name, :avatar_url)
   end
